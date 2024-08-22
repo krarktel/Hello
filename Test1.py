701c37b1b95751de5b572bde5b702efdf31c1d04
krarktel <52972778+krarktel@users.noreply.github.com>
2024-08-22 10:36:14 -0400

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -28,12 +28,12 @@ browser.select_form(nr=0)
 browser.submit()
 browser.select_form(nr=0)
 browser.submit()
-"""if soup.find(string="6 characters long") != None:"""
-print(soup.find(string="6 characters long")
-"""elif soup.find(string="8 characters long") != None:"""
-print(soup.find(string="8 characters long")
-"""else:"""
-"""  y == 0"""
+if soup.find(string='6 characters long') != None:
+  print(soup.find(string='6 characters long')
+elif soup.find(string='8 characters long') != None:
+  print(soup.find(string='8 characters long')
+else:
+  y == 0
 
 browser.form['n'] = reset
 browser.submit()
